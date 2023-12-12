809055ff656866cedb0d5549a112185afcdcb08b
danielcristho <pepuhodaniel93.learn@gmail.com>
2023-12-12 18:03:21 +0700

    feat: add setup project

---
+++ src/config.php
@@ -0,0 +1,12 @@
+<?php
+$server = "172.23.0.2";
+$user = "root";
+$password = "secret";
+$nama_database = "pweb";
+
+$db = mysqli_connect($server, $user, $password, $nama_database);
+
+if( !$db ){
+    die("Gagal terhubung dengan database: " . mysqli_connect_error());
+}
+?>
